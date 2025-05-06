2b69d8141ef469e3c2d27121fe780ad63264a4a5
Dmitry Boyarintsev <skalogryz.lists@gmail.com>
2025-05-06 15:56:29 -0400

    additional conversion the data

--- nival/ether1/ether1res.pas
+++ nival/ether1/ether1res.pas
@@ -22,22 +22,27 @@ type
   end;
 
   TFigHeader = packed record
-    id     : array [0..3] of char; // FIG1
-    count1 : integer;
-    count2 : integer;
-    count3 : integer;
-    vcount : integer; // vertex count
-    count5 : integer;
-    count6 : integer;
-    count7 : integer;
-    count8 : integer;
-    count9 : integer; 
+    id        : array [0..3] of char; // FIG1
+    count1    : integer;
+    count2    : integer; // looks like the same count1
+    uvcount   : integer; // "the number of uvs" before the list of indecis
+    fcount    : integer; // some number that indiates the number of indiices
+    uvcount2  : integer; //  
+    unk1      : integer; // unknown 1 = count1 * 4
+    unk2      : integer; // unknown 2 = always zero?
+    unk3      : integer; // unknown 3 = usually 0x12 or 0x17
+    unk4      : integer; // unknown 4 = 1 or 8 or 0x0D
   end;
+
   TFigVertex = packed record
     x,y,z  : single;
   end;
 
-
+const
+  VtxEntrySize = 12 * 4;
+  VtxExtraSize = 12 * 3 + 4;
+  CntEntrySize = 16 * 4;
+  
 implementation
 
-end.
+end.                                   
--- nival/ether1/fig.pas
+++ nival/ether1/fig.pas
@@ -6,28 +6,85 @@ var
   fs    : TFileStream;
   hdr   : TFigHeader;
   vtx   : array of TFigVertex;
-  i     : integer;
+  i,j   : integer;
+  check : array [0..15] of single;
+  cnt   : integer;  
+  uv    : array [0..1] of single;
+  ff    : array of single;
+  extra : integer;
+  w     : array of word;
 begin
   fs :=TFileStream.Create(fn, fmOpenRead or fmShareDenyWrite);
   try 
     fs.Read(hdr, sizeof(hdr));  
     writeln(hdr.id);
-    writeln(hdr.count1);
-    writeln(hdr.count2);
-    writeln(hdr.count3);
-    writeln('vcount=',hdr.vcount);
-    writeln(hdr.count5);
-    writeln(hdr.count6);
-    writeln(hdr.count7);
-    writeln(hdr.count8);
-    writeln(hdr.count9);
-
+    writeln('count1   = ', hdr.count1);
+    writeln('         = ', hdr.count2);
+    writeln('uvcount  = ', hdr.uvcount);
+    writeln('fcount   = ', hdr.fcount);
+    writeln('uvcount2 = ', hdr.uvcount2);
+    writeln('unk1     = ', hdr.unk1);
+    writeln('unk2     = ', hdr.unk2);
+    writeln('unk2     = ', hdr.unk3);
+    writeln('unk4     = ', hdr.unk4);  
    
-    SetLength(vtx, hdr.vcount);
-    fs.Read(vtx[0], hdr.vcount*sizeof(TFigVertex));
-    for i := 0 to length(vtx)-1 do begin
-      writeln(i,' ', vtx[i].x:0:6,' ',vtx[i].y:0:6,' ',vtx[i].z:0:6);
-    end; 
+    extra := hdr.count1 * VtxEntrySize + VtxExtraSize;
+    SetLength(ff, extra div 4);
+    fs.Read(ff[0], extra);
+    writeln('---prefixed'); 
+    for i := 0 to length(ff)-1 do
+      writeln(ff[i]:0:6);
+
+    j := 1;
+    i := 0;
+    while (i < length(ff)) do begin
+      if (i mod 3 = 0) then begin
+        writeln;
+        writeln('# ', j);
+        write('v ');
+        inc(j);
+      end;
+      write(ff[i]:0:6,' ');
+      inc(i);
+    end;
+    writeln;
+    
+    //fs.Position := fs.Position +  ;
+    writeln('---starting at: ', fs.Position,' ',IntToHex(fs.Position,8),' ---');
+    for i := 0 to hdr.count1-1 do begin
+      fs.Read(check[0], CntEntrySize);
+      write(i,': ');
+      for j := 0 to length(check)-1 do 
+         write(check[j]:0:6,' ');
+      writeln;
+      // writeln(check[12]:0:6 ,' ',check[13]:0:6,' ',check[14]:0:6,' ',check[15]:0:6)
+    end;
+    writeln('--uvs: (',hdr.uvcount,')');
+    for i := 0 to hdr.uvcount-1 do begin
+      fs.Read(uv[0], sizeof(uv));
+      writeln(i,': ', uv[0]:0:6 ,' ',uv[1]:0:6);
+    end;
+
+    cnt := fs.Size-fs.Position;
+    writeln('starting at: ', fs.Position,' ',IntToHex(fs.Position,8));
+    writeln('size left: ' ,cnt);
+    writeln('indecies:  ', cnt div 2);
+    writeln('pairs:     ', cnt div 4);
+    SetLength(w, cnt div 2);
+    fs.Read(w[0], cnt);
+    for i := 0 to length(w)-1 do 
+      writeln(w[i]);
+
+    for i := 0 to hdr.fcount-1 do begin
+      if (i mod 3 = 0) then begin
+        if (i >0) then write(w[i-3]+1);
+        writeln;
+        write('l ');
+      end;
+      write(w[i]+1,' ');
+    end;
+    write(w[hdr.fcount-3]+1);
+    writeln;
   finally 
     fs.free;
   end;
